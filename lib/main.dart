59cc15d41a6dd83f582ac2a19bd3719bbbbb3f34
AbdulReyza <abdulreyza6@gmail.com>
2025-10-27 09:35:23 +0700

    buat material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MyApp);
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,5 +10,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+    return MaterialApp();
   }
 }
