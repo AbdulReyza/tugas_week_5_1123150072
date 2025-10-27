46623f1ffc26e2365f71c10979351bcc4c223a65
AbdulReyza <abdulreyza6@gmail.com>
2025-10-27 09:36:23 +0700

    hilangin banner pjok kanan atas

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
